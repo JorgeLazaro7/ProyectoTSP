1d5e64f81dbbd65a741183ba5d59505e5ce5c5fd
Jorge Alberto Lazaro Arias <wolves@ciencias.unam.mx>
2017-12-03 09:58:51 -0600

    agrego clase de la funcion fitness

---
+++ ComputoEvolutivo/implementacion/src/FitnessFunction/Fitness.java
@@ -0,0 +1,37 @@
+package Function;
+import gaframework.*;
+import org.moeaframework.problem.tsplib.*;
+
+/**
+ * @author Jorge Alberto
+ */
+public class Fitness implements FitnessFunction<Integer>{
+
+
+	DistanceTable table;
+
+	public Fitness(DistanceTable dt){
+		table = dt;
+	}
+
+
+	public double evaluate(Phenotype<Integer> p){
+		double fitness = 0;
+		double distance = distancia(p);
+		fitness = 1/distance;
+		return fitness;
+	}
+
+	public double distancia(Phenotype<Integer> p){
+		double distance = 0;
+		for (int i=0; i < p.size()-1; i++){ 
+		// Calculamos la distancia total de la ruta, sumando las distancias entre 
+		//las ciudades adyacentes en el fenotipo
+			distance += table.getDistanceBetween(p.getAllele(i), p.getAllele(i+1)); 
+		}
+		//falta sumar la distancia entre el la ciudad final y la inicial para cerrar el ciclo
+		distance += table.getDistanceBetween(p.getAllele(p.size()-1), p.getAllele(0));
+		
+		return distance;
+	}
+}
