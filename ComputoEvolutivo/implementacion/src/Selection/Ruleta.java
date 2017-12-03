101fafc70917e14fe430ebd6b7c7b7e36528d8fc
Jorge Alberto Lazaro Arias <wolves@ciencias.unam.mx>
2017-12-03 10:00:20 -0600

    agrego Seleccion por ruleta

---
+++ ComputoEvolutivo/implementacion/src/Selection/Ruleta.java
@@ -0,0 +1,32 @@
+import gaframework.*;
+import java.util.List;
+import java.util.Random;
+import java.util.LinkedList;
+
+public class Ruleta<G,P> implements SelectionOp<G,P>{
+
+	int numSelec;
+
+	public Ruleta(int numSel){
+		numSelec = numSel;
+	}
+
+	public List<Individual<G,P>> select(Population<G,P> p){
+		double[] fit = new double[p.size()];
+		fit[0] = p.getIndividual(0).getFitness();
+		for (int i=0; i < p.size(); i++){
+			fit[i] = fit[i-1] + p.getIndividual(i).getFitness();
+		}
+		Random rdn = new Random(System.currentTimeMillis());
+		LinkedList<Individual<G,P>> seleccion = new LinkedList<>();
+		for(int i=0; i<numSelec; i++){
+			double ex = rdn.nextDouble() * fit[fit.length-1];
+			int j=0;
+			while (j<fit.length-1 && fit[j] <= ex){
+				j++;
+			}
+			seleccion.add(p.getIndividual(j));
+		}
+		return seleccion;
+	}
+}
