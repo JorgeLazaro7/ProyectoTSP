478867b5350bfeaa752446aea8abe490278b3bcd
Jorge Alberto Lazaro Arias <wolves@ciencias.unam.mx>
2017-12-04 05:11:02 -0600

    Cambio la mutacion para que genere individuos válidos

--- ComputoEvolutivo/implementacion/src/Mutation/Mutacion.java
+++ ComputoEvolutivo/implementacion/src/Mutation/Mutacion.java
@@ -9,7 +9,7 @@ public class Mutacion implements MutationOp<Integer>{
 		probMut = probabilidad;
 	}
 
-	public Genotype<Integer> mutate(Genotype<Integer> g){
+	/*public Genotype<Integer> mutate(Genotype<Integer> g){
 		Random r = new Random(System.currentTimeMillis());
 		for(int i = 0; i < g.size(); i++){
 			if (r.nextDouble() <= probMut){
@@ -18,6 +18,76 @@ public class Mutacion implements MutationOp<Integer>{
 			}
 		}
 		return g;
-	}
+	}*/
 
+
+	public Genotype<Integer> mutate(Genotype<Integer> g){
+
+		int size = g.size();
+
+		Random r = new Random(System.currentTimeMillis());
+		//Se crea una mascara de tamano size de booleanos 
+		//true es elegido con probabilidad probMut y 
+		//false es elegido con probabilidad 1-probMut
+        boolean[] mascara = new boolean[size];
+        int aMutar = 0;
+        for (int i=0; i<size; i++){
+        	if (r.nextDouble() <= probMut){
+        		mascara[i] = true;
+        		aMutar++;
+        	}
+        	else
+        		mascara[i] = false;
+        }
+
+        // Si solo tenemos un solo gen a mutar, elegimos otro aleatoriamente para permutarlos
+        // Esto cambia bastante la probabilidad de mutación en el caso de que solo se haya seleccionado uno
+        // Pero nos asegura que se producira un individuo valido
+        while(aMutar == 1){
+        	int pos = r.nextInt(size);
+        	if (!mascara[pos]){
+        		mascara[pos] = true;
+        		aMutar++;
+        	}
+        }
+
+
+        if(aMutar!=0){ 
+
+        	// Ahora hay que permutar los elementos que decidimos mutar, solo los voy a girar a la izquierda,
+        	// Creo que podría mejorar el algoritmo si esa permitacion se hiciera de una manera mas aleatoria.
+
+        	//Creamos un arreglo con las posiciones de los elementos a mutar
+        	int[] posiciones = new int[aMutar];
+        	/*for (int i=0; i<posiciones.length; i++){
+        		for (int j=0; j<mascara.length; j++){
+        			if (mascara[j]){
+        				posiciones[i] = j;
+        				j=mascara.length;
+        			}
+       		 	}
+        	}*/
+
+        	int i = 0;
+        	int p=0;
+        	while(p<posiciones.length){
+        		while (i<mascara.length && !mascara[i]){
+        			i++;
+        		}
+        		posiciones[p] = i;
+        		p++;
+        		i++;
+        	}
+
+    
+
+        	int temp = g.getGene(posiciones[0]);
+        	for (int j=0; j<posiciones.length -1; j++){
+        		g.setGene(posiciones[j], g.getGene(posiciones[j+1]));
+        	}
+        	g.setGene(posiciones[posiciones.length-1], temp);
+        }
+
+        return g;
+	}
 }
